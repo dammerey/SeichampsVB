94f93229af31abcd955f50f0859160a1aa3fdd92
Dammerey <maxime@dammerey.fr>
2025-08-26 14:04:22 +0200

    enlever "release" du suivi

--- app/release/telecharger.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nomFichier = "SeichampsVB_2.0_20250818.apk"; // ton APK
-header("Content-Type: application/vnd.android.package-archive");
-header("Content-Disposition: attachment; filename=\"$nomFichier\"");
-header("Content-Length: " . filesize($nomFichier));
-readfile($nomFichier);
-exit;
-?>
